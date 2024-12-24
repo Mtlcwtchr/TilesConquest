72458a4346646bfaa2f40b579fefd07764bb6a40
mtlcwtchr <aliquaminimicus@gmail.com>
2024-12-24 21:42:21 +0100

    - first camera movement

---
+++ Assets/Scripts/Controls/Camera/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using Utils;
+
+namespace Controls
+{
+	public class CameraController : MonoBehaviour
+	{
+		[SerializeField] private Camera camera;
+		[SerializeField] private float speed;
+		
+		private const float _mouseMoveDelta = 10 * 10;
+		
+		private bool _mousePressed;
+		private Vector2 _mousePosition;
+		private Vector2 _pressPosition;
+
+		private Vector3 _moveDelta;
+		
+		private void Update()
+		{
+			if (Input.GetMouseButtonDown(1))
+			{
+				_mousePressed = true;
+				_pressPosition = Input.mousePosition;
+				_mousePosition = _pressPosition;
+			}
+
+			if (Input.GetMouseButtonUp(1))
+			{
+				_mousePressed = false;
+				_moveDelta = Vector3.zero;
+			}
+
+			if (!_mousePressed)
+				return;
+
+			_mousePosition = Input.mousePosition;
+			_moveDelta = (_pressPosition - _mousePosition).V3();
+			_moveDelta.Normalize();
+		}
+
+		private void FixedUpdate()
+		{
+			if (_moveDelta == Vector3.zero)
+				return;
+			
+			var newPosition = camera.transform.position + _moveDelta * (Time.deltaTime * speed);
+			camera.transform.position = newPosition;
+		}
+	}
+}
---
+++ Assets/Scripts/Utils/VectorUtils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+	public static class VectorUtils
+	{
+		public static Vector2 V2(this Vector3 src)
+		{
+			return new Vector2(src.x, src.z);
+		}
+
+		public static Vector3 V3(this Vector2 src, float y = 0)
+		{
+			return new Vector3(src.x, y, src.y);
+		}
+	}
+}
